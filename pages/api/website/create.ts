866edc51aebec6d8808eb76c72eaa62108df7fd7
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2024-01-17 18:46:56 +0900

    多言語化による新規作成api更新

--- pages/api/website/create.ts
+++ pages/api/website/create.ts
@@ -3,7 +3,7 @@
 | 新しくウェブサイトを作成する
 |--------------------------------------------------------------------------
 */
-import prisma from "../../../lib/prisma";
+import prisma from '../../../lib/prisma';
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -15,6 +15,14 @@ export default async function handle(
     data: {
       // ここにWebsiteの初期データを設定します
       title: "新しいLP",
+      localizedHtml: {
+        create: [
+          { language: "JP", content: "" },
+          { language: "EN", content: "" },
+          { language: "TW", content: "" },
+          { language: "CN", content: "" },
+        ],
+      },
     },
   });
 
